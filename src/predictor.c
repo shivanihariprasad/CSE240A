65ceac41542523324cf6bd1599add97306f6ecdc
Shivani Hariprasad <36236314+shivanihariprasad@users.noreply.github.com>
2022-11-13 10:53:42 -0800

    Implemented gshare algorithm

--- src/predictor.c
+++ src/predictor.c
@@ -11,9 +11,9 @@
 //
 // TODO:Student Information
 //
-const char *studentName = "NAME";
-const char *studentID   = "PID";
-const char *email       = "EMAIL";
+const char *studentName = "Shivani Hariprasad";
+const char *studentID   = "A59019698";
+const char *email       = "shariprasad@ucsd.edu";
 
 //------------------------------------//
 //      Predictor Configuration       //
@@ -29,14 +29,63 @@ int pcIndexBits;  // Number of bits used for PC index
 int bpType;       // Branch Prediction Type
 int verbose;
 
-//------------------------------------//
-//      Predictor Data Structures     //
-//------------------------------------//
+//Data Structures for gshare
+uint8_t *gshare_bht;
+uint64_t global_history; //history
 
+//Helper Functions
+void prediction_helper(uint8_t *bht, uint32_t index, uint8_t outcome){
+  uint8_t prediction = bht[index];
+  if(prediction == WN){
+    bht[index] = (outcome == NOTTAKEN) ? SN : WT;
+  }
+  else if (prediction == SN){
+    bht[index] = (outcome == NOTTAKEN) ? SN : WN;
+  }
+  else if (prediction == WT){
+    bht[index] = (outcome == NOTTAKEN) ? WN : ST;
+  }
+  else if (prediction == ST){
+    bht[index] = (outcome == NOTTAKEN) ? WT : ST
+  }
+}
+
+
+
+//Init Function for ghare
+void init_gshare(){
+  int gshare_bht_size = 1 << ghistoryBits;
+  gshare_bht = malloc((gshare_bht_size * sizeof(uint8_t));
+  memset(gshare_bht, WN, (gshare_bht_size * sizeof(uint8_t));
+  global_history = 0;
+
+}
 //
 //TODO: Add your own Branch Predictor data structures here
 //
 
+uint8_t fetch_gshare_prediction(uint32_t pc){
+  uint32_t gshare_bht_size = 1 << ghistoryBits;
+  uint32_t gshare_bht_index = (global_history ^ pc) & (gshare_bht_size - 1);
+  uint8_t gshare_prediction = gshare_bht[gshare_bht_index];
+  uint8_t outcome;
+  if(gshare_prediction == WN || gshare_prediction == SN){
+    outcome = NOTTAKEN;
+  }
+  else{
+    outcome = TAKEN;
+  }
+  return outcome;
+}
+
+void training_gshare(uint32_t pc, uint8_t outcome){
+  uint32_t gshare_bht_size = 1 << ghistoryBits;
+  uint32_t gshare_bht_index = (global_history ^ pc) & (gshare_bht_size - 1);
+  prediction_helper(gshare_bht, gshare_bht_index, outcome);
+  //Updating the global register
+  ghistory = (ghistory << 1);
+  ghistory = (ghistory  | outcome);
+}
 
 //------------------------------------//
 //        Predictor Functions         //
